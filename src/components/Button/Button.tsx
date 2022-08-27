d13295cad131c021a925dd88ed4f8952a89cb49d
Ang Lee <ang.lee@gmail.com>
2022-08-27 09:38:50 -0400

    Add prettier

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
 
 export interface ButtonProps {
   label: string;
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return <button>{props.label} 5678</button>;
 };
 
 export default Button;
